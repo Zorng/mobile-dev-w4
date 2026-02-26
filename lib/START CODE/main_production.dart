fc8faada449da75be896575fe4d1f27048c452cf
Zorng <ieangzorngl@gmail.com>
2026-02-26 12:34:28 +0700

    Repository Design Pattern

---
+++ lib/START CODE/main_production.dart
@@ -0,0 +1,14 @@
+import 'package:provider/provider.dart';
+import 'data/repositories/songs/song_repository_remote.dart';
+
+import 'data/repositories/songs/song_repository.dart';
+import 'main_common.dart';
+
+/// Configure provider dependencies for dev environement
+List<Provider> get providersLocal {
+  return [Provider<SongRepository>(create: (context) => SongRepositoryRemote())];
+}
+
+void main() {
+  mainCommon(providersLocal);
+}
